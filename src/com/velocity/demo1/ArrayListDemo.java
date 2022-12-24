c106f80d788a23da4befe7fdd43fd836911b2407
sarita-bharit <sari.bharit08@gmail.com>
2022-12-24 17:55:09 +0530

    first commit: added arraylist demo code

---
+++ src/com/velocity/demo1/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity.demo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("sarita");
+		al.add("pooja");
+		al.add("neha");
+		
+		System.out.println(al);
+	} 
+
+}
